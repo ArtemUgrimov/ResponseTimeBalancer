820fd9ee1589ae34759e39660167a3dcaf7cdfc7
a.uhrimov <a.uhrimov@redgenn.com>
2024-08-02 17:52:07 +0200

    Innitial commit

---
+++ main.go
@@ -0,0 +1,105 @@
+package ResponseTimeBalancer
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Config the plugin configuration.
+type Config struct {
+	CookieName        string `json:"cookie_name,omitempty"`
+	ResponseTimeLimit int64  `json:"response_time_limit_ms,omitempty"`
+}
+
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{
+		CookieName:        "pod-id",
+		ResponseTimeLimit: 50,
+	}
+}
+
+// ResponseTimeLimit a ResponseTimeLimit plugin.
+type ResponseTimeLimit struct {
+	next              http.Handler
+	name              string
+	CookieName        string
+	ResponseTimeLimit int64
+}
+
+// New created a new Demo plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	return &ResponseTimeLimit{
+		next:              next,
+		name:              name,
+		CookieName:        config.CookieName,
+		ResponseTimeLimit: config.ResponseTimeLimit,
+	}, nil
+}
+
+func (a *ResponseTimeLimit) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	tsBefore := time.Now().UnixMilli()
+
+	myWriter := &responseWriter{
+		writer:            rw,
+		CookieName:        a.CookieName,
+		ResponseTimeLimit: a.ResponseTimeLimit,
+		startTime:         tsBefore,
+	}
+
+	a.next.ServeHTTP(myWriter, req)
+
+	// tsAfter := time.Now().UnixMilli()
+	// os.Stderr.WriteString(fmt.Sprintf("Response time = %d", tsAfter-tsBefore))
+}
+
+type responseWriter struct {
+	writer            http.ResponseWriter
+	CookieName        string
+	ResponseTimeLimit int64
+
+	startTime int64
+}
+
+func (r *responseWriter) Header() http.Header {
+	return r.writer.Header()
+}
+
+func (r *responseWriter) Write(bytes []byte) (int, error) {
+	return r.writer.Write(bytes)
+}
+
+func (r *responseWriter) WriteHeader(statusCode int) {
+
+	tsAfter := time.Now().UnixMilli()
+	os.Stderr.WriteString(fmt.Sprintf("Response time = %d", tsAfter-r.startTime))
+
+	if tsAfter-r.startTime > r.ResponseTimeLimit {
+		// Delete set-cookie headers
+		r.writer.Header().Del(r.CookieName)
+
+		os.Stderr.WriteString(fmt.Sprintf("Delete cookies because of response time = %d", tsAfter-r.startTime))
+	}
+
+	r.writer.WriteHeader(statusCode)
+}
+
+func (r *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hijacker, ok := r.writer.(http.Hijacker)
+	if !ok {
+		return nil, nil, fmt.Errorf("%T is not a http.Hijacker", r.writer)
+	}
+
+	return hijacker.Hijack()
+}
+
+func (r *responseWriter) Flush() {
+	if flusher, ok := r.writer.(http.Flusher); ok {
+		flusher.Flush()
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,46 @@
+package ResponseTimeBalancer_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ArtemUgrimov/ResponseTimeBalancer"
+)
+
+func TestDemo(t *testing.T) {
+	cfg := ResponseTimeBalancer.CreateConfig()
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	handler, err := ResponseTimeBalancer.New(ctx, next, cfg, "ResponseTimeBalancer-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.AddCookie(&http.Cookie{
+		Name:  "pod-id",
+		Value: "111",
+	})
+
+	handler.ServeHTTP(recorder, req)
+
+	assertCookie(t, req)
+}
+
+func assertCookie(t *testing.T, req *http.Request) {
+	t.Helper()
+
+	_, err := req.Cookie("pod-id")
+	if err == nil {
+		t.Errorf("pod-id cookie is present")
+	}
+}
