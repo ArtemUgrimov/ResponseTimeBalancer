a9a1a34956c7bddf417fcc89e45e9b7e7035f110
a.uhrimov <a.uhrimov@redgenn.com>
2024-08-05 13:34:28 +0200

    Updated plugin

--- main.go
+++ main.go
@@ -7,69 +7,63 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"time"
+	"strconv"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	CookieName        string `json:"cookie_name,omitempty"`
-	ResponseTimeLimit int64  `json:"response_time_limit_ms,omitempty"`
+	CookieName             string `json:"cookie_name,omitempty"`
+	ResponseTimeHeaderName string `json:"response_time_header_name,omitempty"`
+	ResponseTimeLimit      int    `json:"response_time_limit_ms,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		CookieName:        "pod-id",
-		ResponseTimeLimit: 50,
+		CookieName:             "pod-id",
+		ResponseTimeHeaderName: "Tm",
+		ResponseTimeLimit:      50,
 	}
 }
 
 // ResponseTimeLimit a ResponseTimeLimit plugin.
 type ResponseTimeLimit struct {
-	next              http.Handler
-	name              string
-	CookieName        string
-	ResponseTimeLimit int64
+	next                   http.Handler
+	name                   string
+	CookieName             string
+	ResponseTimeHeaderName string
+	ResponseTimeLimit      int
 }
 
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	os.Stderr.WriteString(fmt.Sprintf("Creating a ResponseTimeLimit plugin. Config : %v\n", config))
+
 	return &ResponseTimeLimit{
-		next:              next,
-		name:              name,
-		CookieName:        config.CookieName,
-		ResponseTimeLimit: config.ResponseTimeLimit,
+		next:                   next,
+		name:                   name,
+		CookieName:             config.CookieName,
+		ResponseTimeHeaderName: config.ResponseTimeHeaderName,
+		ResponseTimeLimit:      config.ResponseTimeLimit,
 	}, nil
 }
 
 func (a *ResponseTimeLimit) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	tsBefore := time.Now().UnixMilli()
-
 	myWriter := &responseWriter{
-		writer:            rw,
-		CookieName:        a.CookieName,
-		ResponseTimeLimit: a.ResponseTimeLimit,
-		startTime:         tsBefore,
+		writer:                 rw,
+		CookieName:             a.CookieName,
+		ResponseTimeHeaderName: a.ResponseTimeHeaderName,
+		ResponseTimeLimit:      a.ResponseTimeLimit,
 	}
 
-	os.Stderr.WriteString("\n\n\nBEFORE SERVE\n\n\n")
-
 	a.next.ServeHTTP(myWriter, req)
-	// a.next.ServeHTTP(rw, req)
-
-	os.Stderr.WriteString("\n\n\nAFTER SERVE\n\n\n")
-
-	tsAfter := time.Now().UnixMilli()
-	os.Stderr.WriteString(fmt.Sprintf("Response time = %d", tsAfter-tsBefore))
-	os.Stdout.WriteString(fmt.Sprintf("Response time = %d", tsAfter-tsBefore))
 }
 
 type responseWriter struct {
-	writer            http.ResponseWriter
-	CookieName        string
-	ResponseTimeLimit int64
-
-	startTime int64
+	writer                 http.ResponseWriter
+	CookieName             string
+	ResponseTimeHeaderName string
+	ResponseTimeLimit      int
 }
 
 func (r *responseWriter) Header() http.Header {
@@ -81,17 +75,21 @@ func (r *responseWriter) Write(bytes []byte) (int, error) {
 }
 
 func (r *responseWriter) WriteHeader(statusCode int) {
-
-	tsAfter := time.Now().UnixMilli()
-	os.Stderr.WriteString(fmt.Sprintf("Response time = %d", tsAfter-r.startTime))
-
-	r.writer.Header().Add("Poceluj moju zalupu", "1488")
-	// r.writer.Header().Del(r.CookieName)
-	// if tsAfter-r.startTime > r.ResponseTimeLimit {
-	// 	// Delete set-cookie headers
-
-	// 	os.Stderr.WriteString(fmt.Sprintf("Delete cookies because of response time = %d", tsAfter-r.startTime))
-	// }
+	tmStr := r.writer.Header().Get(r.ResponseTimeHeaderName)
+	if len(tmStr) > 0 {
+		os.Stderr.WriteString(fmt.Sprintf("Response time = %s\n", tmStr))
+		tm, err := strconv.Atoi(tmStr)
+		if err == nil {
+			if tm > r.ResponseTimeLimit {
+				r.writer.Header().Set("Set-Cookie", fmt.Sprintf("%s=invalidated", r.CookieName))
+				os.Stderr.WriteString(fmt.Sprintf("Deleting cookie with name %s\n", r.CookieName))
+			} else {
+				os.Stderr.WriteString(fmt.Sprintf("Limit (%d) is not reached. Skip\n", r.ResponseTimeLimit))
+			}
+		}
+	} else {
+		os.Stderr.WriteString(fmt.Sprintf("Could not find header %s", r.ResponseTimeHeaderName))
+	}
 
 	r.writer.WriteHeader(statusCode)
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 Dynamic :
 
+yaml```
 http:
   # https://doc.traefik.io/traefik/middlewares/http/overview/
   middlewares:
@@ -8,3 +9,6 @@ http:
         rtb_static:
           cookie_name: "pod-id"
           response_time_limit_ms: 50
+          response_time_header_name: "Tm"
+          max_age_value: "Session"
+```
